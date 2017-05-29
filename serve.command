49998ad75b397e277a00eb6eedbba6b9727b113c
Lukas Hermann <lukher6@web.de>
2017-05-29 16:06:30 +0200

    Removed rsync script

--- serve.command
+++
@@ -1,2 +0,0 @@
-cd "`dirname "$0"`"
-bundle exec jekyll serve
--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# rm -r _site
-
-bundle exec jekyll build
-
-rsync -avz -L --stats --rsh='ssh -p22007' ~/htdocs/lmphilippines/_site/ lukas@host1.codethink.de:/var/customers/webs/codethink/lmphilippines.org
