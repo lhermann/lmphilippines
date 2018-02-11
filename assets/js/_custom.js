e6f9e14caa5ac8b5ae28ca4715be4bfa894d66d5
Lukas Hermann <lukher6@web.de>
2018-02-11 22:29:05 +0100

    WIP landing page

--- assets/js/_custom.js
+++ assets/js/_custom.js
@@ -2,21 +2,21 @@
     $(".u-zoom").on("click", function() {
         $(this).toggleClass("is-zoomed");
     });
-
-    // $.featherlightGallery.prototype.afterContent = function() {
-    //     var caption = this.$currentTarget.find("img").attr("alt");
-    //     this.$instance.find(".featherlight-caption").remove();
-    //     if (caption) {
-    //         $('<div class="featherlight-caption">')
-    //             .text(caption)
-    //             .appendTo(this.$instance.find(".featherlight-content"));
-    //     }
-    // };
-
-    // $(".jsFeatherlightGallery a").featherlightGallery({
-    //     previousIcon:
-    //         '<spanc class="u-ic u-ic--arrow-left"></span>' /* Code that is used as previous icon */,
-    //     nextIcon:
-    //         '<spanc class="u-ic u-ic--arrow-right"></span>' /* Code that is used as next icon */
-    // });
 })();
+
+$.featherlightGallery.prototype.afterContent = function() {
+    var caption = this.$currentTarget.find("img").attr("alt");
+    this.$instance.find(".featherlight-caption").remove();
+    if (caption) {
+        $('<div class="featherlight-caption">')
+            .text(caption)
+            .appendTo(this.$instance.find(".featherlight-content"));
+    }
+};
+
+$(".jsFeatherlightGallery a").featherlightGallery({
+    previousIcon:
+        '<spanc class="u-ic u-ic--arrow-left"></span>' /* Code that is used as previous icon */,
+    nextIcon:
+        '<spanc class="u-ic u-ic--arrow-right"></span>' /* Code that is used as next icon */
+});
--- assets/js/lmp.js
+++ assets/js/lmp.js
@@ -10,6 +10,22 @@
     {% include_relative vendor/featherlight.gallery.min.js %}
 {% endcomment %}
 
+/*
+ * Lazysizes config
+ * This object must be defined before the lazysizes script.
+ */
+// window.lazySizesConfig = window.lazySizesConfig || {};
+// window.lazySizesConfig.lazyClass = 'u-lazyload';
+// window.lazySizesConfig.preloadClass = 'u-lazypreload';
+// window.lazySizesConfig.loadingClass = 'u-lazyloading';
+// window.lazySizesConfig.loadedClass = 'u-lazyloaded';
+
+{% include_relative vendor/lazysizes.min.js %}
+
+{% include_relative vendor/jquery.detect_swipe.js %}
+{% include_relative vendor/featherlight.min.js %}
+{% include_relative vendor/featherlight.gallery.min.js %}
+
 {% include_relative _mobile-nav.js %}
 {% include_relative _spambotscare.js %}
 
